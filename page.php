ffa3ebadc87a5de7f45fa4b815fa12acc9495ff9
Koutarou ISHIKAWA <stein2nd@gmail.com>
2025-12-27 22:47:53 +0900

    Create page.php

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php
+	get_header();
+    remove_filter('the_content', 'wpautop');
+?>
+<div class="pageContent">
+	<div class="fixedWidth spPad breadcrumb">
+		<?php breadcrumb($post); ?>
+	</div>
+	<div>
+		<header class="pageTitle fixedWidth">
+			<h1 class="string"><?php the_title(); ?></h1>
+			<?php if( get_field('en_title', $post->ID) ) : ?>
+				<small><?php the_field('en_title', $post->ID); ?></small>
+			<?php endif; ?>
+		</header>
+		<?php
+			while ( have_posts() ) : the_post();
+				the_content();
+			endwhile;
+		?>
+	</div>
+</div>
+<?php
+get_footer();
