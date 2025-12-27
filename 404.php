110835203a2070d446872825a354d4150a0c7f48
Koutarou ISHIKAWA <stein2nd@gmail.com>
2025-12-27 22:49:45 +0900

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="pageContent">
+	<div class="fixedWidth spPad breadcrumb">
+		<?php breadcrumb($post); ?>
+	</div>
+	<div class="fixedWidth spPad">
+		<header class="pageTitle">
+			<h1 class="string">ページがみつかりませんでした</h1>
+			<small>NOT FOUND</small>
+		</header>
+		<div class="setAlignC">
+			<p>ページがみつかりませんでした</p>
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
