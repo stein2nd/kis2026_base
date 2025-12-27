522a87c6ff53a1722b41de79f91f59c42f859684
Koutarou ISHIKAWA <stein2nd@gmail.com>
2025-12-27 22:48:19 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,108 @@
+</div>
+<div class="siteFooter">
+    <div class="siteFooterButton spPad">
+        <div class="fixedWidth">
+            <section>
+                <header class="siteFooterButton__title">
+                    <h2 class="string">＼国際物流システムのことならKISにおまかせください／</h2>
+                </header>
+                <ul class="siteFooterButton__content">
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>inquiry/" class="button button--b">
+								<span class="item">
+									<span class="icon icon--catalog"></span>資料請求<span
+                                        class="icon icon--arrowLBL"></span>
+								</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>inquiry/?type=contact" class="button button--b">
+								<span class="item">
+									<span class="icon icon--mail"></span>お問い合わせ<span class="icon icon--arrowLBL"></span>
+								</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section class="sp">
+                <header class="siteFooterButton__title">
+                    <h2 class="string">お電話でのお問い合わせはこちら</h2>
+                </header>
+                <ul class="siteFooterButton__content">
+                    <li>
+                        <div class="button button--d">
+								<span class="item">
+									<span class="icon icon--telW"></span>大阪：<a
+                                        href="tel:06-4706-1236"><b>06-4706-1236</b></a>
+								</span>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+            <a href="#wrapper" class="button buttonToTop"></a>
+        </div>
+    </div>
+    <div class="siteFooterInfo col">
+        <div class="fixedWidth flex">
+            <div class="flex__item item">
+                <ul class="siteFooterInfoLink">
+                    <li><a href="<?php echo esc_url(home_url('/')); ?>reason/"><span class="icon icon--arrow"></span>KISが選ばれる理由</a>
+                    </li>
+                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>news/"><span
+                                class="icon icon--arrow"></span>ニュース</a></li>
+                    <li><a href="<?php echo esc_url(home_url('/')); ?>event/"><span class="icon icon--arrow"></span>イベント</a>
+                    </li>
+                    <li class="pc"><a href="<?php echo esc_url(home_url('/')); ?>inquiry/"><span
+                                class="icon icon--arrow"></span>お問い合わせ・資料請求</a></li>
+                </ul>
+            </div>
+            <div class="flex__item item">
+                <ul class="siteFooterInfoLink">
+                    <li class="spEven">
+                        <a href="<?php echo esc_url(home_url('/')); ?>products/"><span class="icon icon--arrow"></span>製品紹介</a>
+                        <ul>
+                            <li><a href="<?php echo esc_url(home_url('/')); ?>products/forwarderpro/">Forwarder-PROシリーズ</a></li>
+                            <li><a href="<?php echo esc_url(home_url('/')); ?>products/solution/">各種ソリューション</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="<?php echo esc_url(home_url('/')); ?>case/"><span class="icon icon--arrow"></span>導入事例</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex__item item">
+                <ul class="siteFooterInfoLink">
+                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>corporate/"><span
+                                class="icon icon--arrow"></span>会社情報</a></li>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>recruit/"><span class="icon icon--arrow"></span>採用情報</a>
+                    </li>
+                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>privacy/"><span
+                                class="icon icon--arrow"></span>個人情報保護方針</a></li>
+                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>informationsecurity/"><span
+                                class="icon icon--arrow"></span>情報ｾｷｭﾘﾃｨ基本方針</a></li>
+                    <!--
+                    <li class="spEven">
+                        <script type="text/javascript" src="//seal.securecore.co.jp/js/ss_130-50.js"></script><noscript><img src="//seal.securecore.co.jp/image/noscript_130x50.png"></noscript>
+                    </li>
+                    -->
+                </ul>
+            </div>
+            <div class="flex__item item pc">
+                <div class="logoSmall"><img
+                        src="<?php echo get_template_directory_uri(); ?>/dist/assets/logo_w.svg"
+                        alt="関西総合システム株式会社"></div>
+                <div><a href="<?php echo esc_url(home_url('/')); ?>inquiry/" class="button button--c">資料請求・お問い合わせ</a>
+                </div>
+                <ul class="siteFooterInfoTel">
+                    <li><span class="icon icon--tel"></span>大阪：<b>06-4706-1236</b></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <p class="copy">&copy;2020 Kansai Integrated Systems CO.,LTD. All rights reserved.</p>
+</div>
+<?php wp_footer(); ?>
+<div class="pc"></div>
+</div>
+</body>
+</html>
