e5ab1b3a6d9f306439ea2b22020d8d13f525f620
Koutarou ISHIKAWA <stein2nd@gmail.com>
2026-04-08 21:54:58 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,17 +9,12 @@
                 <ul class="siteFooterButton__content">
                     <li>
                         <a href="<?php echo esc_url(home_url('/')); ?>inquiry/" class="button button--b">
-								<span class="item">
-									<span class="icon icon--catalog"></span>資料請求<span
-                                        class="icon icon--arrowLBL"></span>
-								</span>
+								<span class="item"><span class="icon icon--catalog"></span>資料請求<span class="icon icon--arrowLBL"></span></span>
                         </a>
                     </li>
                     <li>
                         <a href="<?php echo esc_url(home_url('/')); ?>inquiry/?type=contact" class="button button--b">
-								<span class="item">
-									<span class="icon icon--mail"></span>お問い合わせ<span class="icon icon--arrowLBL"></span>
-								</span>
+								<span class="item"><span class="icon icon--mail"></span>お問い合わせ<span class="icon icon--arrowLBL"></span></span>
                         </a>
                     </li>
                 </ul>
@@ -31,10 +26,7 @@
                 <ul class="siteFooterButton__content">
                     <li>
                         <div class="button button--d">
-								<span class="item">
-									<span class="icon icon--telW"></span>大阪：<a
-                                        href="tel:06-4706-1236"><b>06-4706-1236</b></a>
-								</span>
+								<span class="item"><span class="icon icon--telW"></span>大阪：<a href="tel:06-4706-1236"><b>06-4706-1236</b></a></span>
                         </div>
                     </li>
                 </ul>
@@ -46,14 +38,18 @@
         <div class="fixedWidth flex">
             <div class="flex__item item">
                 <ul class="siteFooterInfoLink">
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>reason/"><span class="icon icon--arrow"></span>KISが選ばれる理由</a>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>reason/"><span class="icon icon--arrow"></span>KISが選ばれる理由</a>
                     </li>
-                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>news/"><span
-                                class="icon icon--arrow"></span>ニュース</a></li>
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>event/"><span class="icon icon--arrow"></span>イベント</a>
+                    <li class="spEven">
+                        <a href="<?php echo esc_url(home_url('/')); ?>news/"><span class="icon icon--arrow"></span>ニュース</a>
                     </li>
-                    <li class="pc"><a href="<?php echo esc_url(home_url('/')); ?>inquiry/"><span
-                                class="icon icon--arrow"></span>お問い合わせ・資料請求</a></li>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>event/"><span class="icon icon--arrow"></span>イベント</a>
+                    </li>
+                    <li class="pc">
+                        <a href="<?php echo esc_url(home_url('/')); ?>inquiry/"><span class="icon icon--arrow"></span>お問い合わせ・資料請求</a>
+                    </li>
                 </ul>
             </div>
             <div class="flex__item item">
@@ -65,21 +61,33 @@
                             <li><a href="<?php echo esc_url(home_url('/')); ?>products/solution/">各種ソリューション</a></li>
                         </ul>
                     </li>
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>case/"><span class="icon icon--arrow"></span>導入事例</a>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>case/"><span class="icon icon--arrow"></span>導入事例</a>
                     </li>
                 </ul>
             </div>
             <div class="flex__item item">
                 <ul class="siteFooterInfoLink">
-                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>corporate/"><span
-                                class="icon icon--arrow"></span>会社情報</a></li>
-                    <li>
-                        <a href="<?php echo esc_url(home_url('/')); ?>recruit/"><span class="icon icon--arrow"></span>採用情報</a>
+                    <li class="spEven">
+                        <a href="<?php echo esc_url(home_url('/')); ?>corporate/">
+                            <span class="icon icon--arrow"></span>会社情報
+                        </a>
                     </li>
-                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>privacy/"><span
-                                class="icon icon--arrow"></span>個人情報保護方針</a></li>
-                    <li class="spEven"><a href="<?php echo esc_url(home_url('/')); ?>informationsecurity/"><span
-                                class="icon icon--arrow"></span>情報ｾｷｭﾘﾃｨ基本方針</a></li>
+                    <li>
+                        <a href="<?php echo esc_url(home_url('/')); ?>recruit/">
+                            <span class="icon icon--arrow"></span>採用情報
+                        </a>
+                    </li>
+                    <li class="spEven">
+                        <a href="<?php echo esc_url(home_url('/')); ?>privacy/">
+                            <span class="icon icon--arrow"></span>個人情報保護方針
+                        </a>
+                    </li>
+                    <li class="spEven">
+                        <a href="<?php echo esc_url(home_url('/')); ?>informationsecurity/">
+                            <span class="icon icon--arrow"></span>情報ｾｷｭﾘﾃｨ基本方針
+                        </a>
+                    </li>
                     <!--
                     <li class="spEven">
                         <script type="text/javascript" src="//seal.securecore.co.jp/js/ss_130-50.js"></script><noscript><img src="//seal.securecore.co.jp/image/noscript_130x50.png"></noscript>
@@ -88,13 +96,16 @@
                 </ul>
             </div>
             <div class="flex__item item pc">
-                <div class="logoSmall"><img
-                        src="<?php echo get_template_directory_uri(); ?>/dist/assets/logo_w.svg"
-                        alt="関西総合システム株式会社"></div>
-                <div><a href="<?php echo esc_url(home_url('/')); ?>inquiry/" class="button button--c">資料請求・お問い合わせ</a>
+                <div class="logoSmall">
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/logo_w.svg" alt="関西総合システム株式会社">
+                </div>
+                <div>
+                    <a href="<?php echo esc_url(home_url('/')); ?>inquiry/" class="button button--c">資料請求・お問い合わせ</a>
                 </div>
                 <ul class="siteFooterInfoTel">
-                    <li><span class="icon icon--tel"></span>大阪：<b>06-4706-1236</b></li>
+                    <li>
+                        <span class="icon icon--tel"></span>大阪：<b>06-4706-1236</b>
+                    </li>
                 </ul>
             </div>
         </div>
